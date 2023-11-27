07a7a5d67bb6e7aff030a29c7cc85e62eadb2a69
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-11-27 14:25:01 +0200

    Add redis configuration

---
+++ src/main/java/com/omarahmed42/socialmedia/configuration/RedisConfiguration.java
@@ -0,0 +1,32 @@
+package com.omarahmed42.socialmedia.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private Integer redisPort;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate() {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setEnableTransactionSupport(true);
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
