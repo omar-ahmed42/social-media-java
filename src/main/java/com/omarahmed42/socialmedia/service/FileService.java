2cb3a68eff6d3794ef8bde5614725bacf1e327b5
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-11-27 20:43:04 +0200

    Add File service to support some file system operations
    Add File service to support some file system operations such as copy operation.
    This would make it easier to perform unit tests by mocking FileService rather than static methods from java.nio.file.Files

---
+++ src/main/java/com/omarahmed42/socialmedia/service/FileService.java
@@ -0,0 +1,10 @@
+package com.omarahmed42.socialmedia.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.Path;
+
+public interface FileService {
+    public long copy(InputStream in, Path target, CopyOption... options) throws IOException;
+}
---
+++ src/main/java/com/omarahmed42/socialmedia/service/impl/FileServiceImpl.java
@@ -0,0 +1,19 @@
+package com.omarahmed42.socialmedia.service.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.stereotype.Service;
+
+import com.omarahmed42.socialmedia.service.FileService;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    public long copy(InputStream in, Path target, CopyOption... options) throws IOException {
+        return Files.copy(in, target, options);
+    }
+}
