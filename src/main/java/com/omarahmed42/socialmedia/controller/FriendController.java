65c9593d04f73a8f4017ee67616c1e33dad8e29a
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-11-28 12:29:14 +0200

    Add FriendController to unfriend users and retrieve friends

---
+++ src/main/java/com/omarahmed42/socialmedia/controller/FriendController.java
@@ -0,0 +1,31 @@
+package com.omarahmed42.socialmedia.controller;
+
+import java.util.List;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import com.omarahmed42.socialmedia.dto.PaginationInfo;
+import com.omarahmed42.socialmedia.model.User;
+import com.omarahmed42.socialmedia.service.FriendService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class FriendController {
+
+    private final FriendService friendService;
+
+    @MutationMapping
+    public Boolean unfriend(@Argument Long friendId) {
+        return friendService.unfriend(friendId);
+    }
+
+    @QueryMapping
+    public List<User> findFriends(@Argument Integer page, @Argument Integer pageSize) {
+        return friendService.findFriends(new PaginationInfo(page, pageSize));
+    }
+}
