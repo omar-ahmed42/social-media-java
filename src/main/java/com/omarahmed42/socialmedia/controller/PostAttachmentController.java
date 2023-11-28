c60d0b5a3e67b6b111985c7d6d08cc55fd3b67ca
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-11-28 13:42:42 +0200

    Add post attachment controller to upload attachments

---
+++ src/main/java/com/omarahmed42/socialmedia/controller/PostAttachmentController.java
@@ -0,0 +1,36 @@
+package com.omarahmed42.socialmedia.controller;
+
+import java.net.URI;
+
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.omarahmed42.socialmedia.model.Post;
+import com.omarahmed42.socialmedia.model.PostAttachment;
+import com.omarahmed42.socialmedia.service.PostAttachmentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class PostAttachmentController {
+
+    private final PostAttachmentService postAttachmentService;
+
+    @PostMapping("/api/v1/posts/{post-id}/attachments")
+    public ResponseEntity<PostAttachment> addPostAttachment(MultipartFile multipartFile,
+            @PathVariable("post-id") Long postId) {
+        Long id = postAttachmentService.savePostAttachment(multipartFile, postId);
+        return ResponseEntity.created(URI.create("/api/v1/posts/" + postId + "/attachments/" + id)).build();
+    }
+
+    @SchemaMapping(typeName = "PostAttachment", field = "post")
+    public Post post(PostAttachment postAttachment) {
+        return postAttachment.getPostAttachmentId().getPost();
+    }
+
+}
