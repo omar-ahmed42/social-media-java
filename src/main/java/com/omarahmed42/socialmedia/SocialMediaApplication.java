f719f9e3c2c90f494ccc4181bd112516f656b5a2
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-05-05 19:05:45 +0300

    Add and Use RedisCacheManager

--- src/main/java/com/omarahmed42/socialmedia/SocialMediaApplication.java
+++ src/main/java/com/omarahmed42/socialmedia/SocialMediaApplication.java
@@ -2,13 +2,11 @@ package com.omarahmed42.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.cassandra.config.EnableCassandraAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
 
 @SpringBootApplication
-@EnableCaching
 @EnableJpaAuditing
 @EnableCassandraAuditing
 @EnableNeo4jAuditing
--- src/main/java/com/omarahmed42/socialmedia/configuration/RedisConfiguration.java
+++ src/main/java/com/omarahmed42/socialmedia/configuration/RedisConfiguration.java
@@ -1,13 +1,19 @@
 package com.omarahmed42.socialmedia.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@EnableCaching
 public class RedisConfiguration {
 
     @Value("${spring.data.redis.host}")
@@ -28,5 +34,11 @@ public class RedisConfiguration {
         template.setEnableTransactionSupport(true);
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory jedisConnectionFactory) {
+        return RedisCacheManager.builder(jedisConnectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()).transactionAware().build();
     }
 }
