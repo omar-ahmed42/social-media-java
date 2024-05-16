5d39aa181ff4a7982d40177a7ca4a9b8a0c1d9ca
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-05-16 14:49:54 +0300

    Update redis configuration to use appropriate serializers and deserializers
    - Update Redis configuration to use appropriate serializers and deserializers as the default serializer/deserializer would fail to deserialize data from cache upon application restart (in cacheManager).
    - As for redisTemplate, use a StringRedisSerializer for key serialization for better readability.

--- src/main/java/com/omarahmed42/socialmedia/configuration/RedisConfiguration.java
+++ src/main/java/com/omarahmed42/socialmedia/configuration/RedisConfiguration.java
@@ -11,6 +11,18 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module.Feature;
 
 @Configuration
 @EnableCaching
@@ -29,16 +41,40 @@ public class RedisConfiguration {
     }
 
     @Bean
-    public RedisTemplate<String, Long> redisTemplate() {
+    public RedisTemplate<String, Long> redisTemplate(ObjectMapper objectMapper) {
+        ObjectMapper mapper = objectMapper
+                .copy()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(mapper, Long.class));
         template.setEnableTransactionSupport(true);
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
     }
 
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory jedisConnectionFactory) {
+    ObjectMapper jacksonObjectMapper() {
+        return new Jackson2ObjectMapperBuilder().build();
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory jedisConnectionFactory, ObjectMapper objectMapper) {
+        ObjectMapper mapper = objectMapper
+                .copy()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                .registerModule(new Hibernate6Module().enable(Feature.REPLACE_PERSISTENT_COLLECTIONS)
+                        .enable(Feature.FORCE_LAZY_LOADING))
+                .activateDefaultTyping(
+                        jacksonObjectMapper().getPolymorphicTypeValidator(),
+                        ObjectMapper.DefaultTyping.EVERYTHING,
+                        JsonTypeInfo.As.PROPERTY);
         return RedisCacheManager.builder(jedisConnectionFactory)
-                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()).transactionAware().build();
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
+                        .serializeValuesWith(
+                                SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(mapper))))
+                .transactionAware().build();
     }
 }
