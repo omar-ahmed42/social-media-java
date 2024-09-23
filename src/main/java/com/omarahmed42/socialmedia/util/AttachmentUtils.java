f129eeed350383d8549b9b7dac8a9df3bc3db431
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-23 12:21:42 +0300

    Create AttachmentUtils.java

---
+++ src/main/java/com/omarahmed42/socialmedia/util/AttachmentUtils.java
@@ -0,0 +1,11 @@
+package com.omarahmed42.socialmedia.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class AttachmentUtils {
+    public static final String[] IMAGE_EXTENSIONS = { "png", "jpg" };
+    public static final String[] VIDEO_EXTENSIONS = { "mp4", "avi", "mkv" };
+
+    public static final String EXTENSION_SEPARATOR = ".";
+}
