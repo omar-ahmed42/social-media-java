a4940b72eaaca391f27f2bb9757a4738c5e7b563
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-23 12:23:11 +0300

    Add transaction template

--- src/main/java/com/omarahmed42/socialmedia/configuration/JpaTxConfiguration.java
+++ src/main/java/com/omarahmed42/socialmedia/configuration/JpaTxConfiguration.java
@@ -1,5 +1,6 @@
 package com.omarahmed42.socialmedia.configuration;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -7,6 +8,7 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Configuration
 @EnableTransactionManagement
@@ -17,5 +19,11 @@ public class JpaTxConfiguration {
     public PlatformTransactionManager jpaTransactionManager(
             LocalContainerEntityManagerFactoryBean entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+
+    @Bean
+    TransactionTemplate transactionTemplate(
+            @Qualifier("transactionManager") PlatformTransactionManager jpaTransactionManager) {
+        return new TransactionTemplate(jpaTransactionManager);
     }
 }
