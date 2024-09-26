d628f4720cc040f3a7e0f02027ccca6f9d4161ff
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-26 15:11:02 +0300

    Implement Persistable<UUID> for RefreshToken to avoid unnecessary SELECT
    - Implement Persistable<UUID> interface in RefreshToken to avoid unnecessary SELECT queries before INSERT operations (As the ID is not auto-generated).

--- src/main/java/com/omarahmed42/socialmedia/model/RefreshToken.java
+++ src/main/java/com/omarahmed42/socialmedia/model/RefreshToken.java
@@ -3,6 +3,8 @@ package com.omarahmed42.socialmedia.model;
 import java.io.Serializable;
 import java.util.UUID;
 
+import org.springframework.data.domain.Persistable;
+
 import com.omarahmed42.socialmedia.enums.TokenStatus;
 
 import jakarta.persistence.Column;
@@ -13,13 +15,18 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Entity
 @Table(name = "refresh_tokens")
 @Data
-public class RefreshToken implements Serializable {
+public class RefreshToken implements Serializable, Persistable<UUID> {
     @Id
     private UUID id;
 
@@ -31,6 +38,20 @@ public class RefreshToken implements Serializable {
     private Long validUntil;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", updatable = false)
     private User user;
+
+    @Transient
+    @Setter(value = AccessLevel.NONE)
+    private boolean isNew = true;
+
+    @PrePersist
+    @PostLoad
+    public void markNotNew() {
+        this.isNew = false;
+    }
+
+    public void markNew() {
+        this.isNew = true;
+    }
 }
