bda501bde6af9f7b1fb60cfb7d1d151592a8699f
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-29 16:14:51 +0300

    Fix a bug that caused username to be null in the response
    - Fix a bug that caused username to be null in the response as the UserPublicInfoDto and UserPersonaInfoDto didn't include the username field, nor did the UserRepository.

--- src/main/java/com/omarahmed42/socialmedia/dto/projection/UserPublicInfoDto.java
+++ src/main/java/com/omarahmed42/socialmedia/dto/projection/UserPublicInfoDto.java
@@ -13,15 +13,17 @@ public class UserPublicInfoDto implements Serializable {
     protected Long id;
     protected String firstName;
     protected String lastName;
+    protected String username;
     protected LocalDate dateOfBirth;
     protected LocalDateTime createdAt;
     protected AttachmentDto avatar;
     protected AttachmentDto coverPicture;
 
-    public UserPublicInfoDto(Long id, String firstName, String lastName, LocalDate dateOfBirth, LocalDateTime createdAt, AttachmentDto avatar, AttachmentDto coverPicture) {
+    public UserPublicInfoDto(Long id, String firstName, String lastName, String username, LocalDate dateOfBirth, LocalDateTime createdAt, AttachmentDto avatar, AttachmentDto coverPicture) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.username = username;
         this.dateOfBirth = dateOfBirth;
         this.createdAt = createdAt;
         this.avatar = avatar;
