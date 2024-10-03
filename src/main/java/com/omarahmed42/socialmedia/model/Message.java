dee3341e865ba008dd3943acf6bb3311d10f1a55
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-10-04 01:20:30 +0300

    Implement Persistable and pluralize table's name for Message
     - Implement Persistable interface to differentiate between entities to be persisted and entities to be updated.
    - Remove unnecessary comments.
    - Pluralize Message's table name (by renaming it from "message' to "messages").

--- src/main/java/com/omarahmed42/socialmedia/model/Message.java
+++ src/main/java/com/omarahmed42/socialmedia/model/Message.java
@@ -5,23 +5,29 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.domain.Persistable;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.omarahmed42.socialmedia.enums.MessageStatus;
 
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PrePersist;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
-@Table
+@Table("messages")
 @Data
-public class Message implements Serializable {
-    // @PrimaryKey
-    // private Long id;
+public class Message implements Persistable<MessagePK>, Serializable {
     @PrimaryKey
-    private MessageId id;
+    @JsonUnwrapped
+    private MessagePK id;
 
     @Column(value = "content")
     private String content;
@@ -36,14 +42,17 @@ public class Message implements Serializable {
     @Column(value = "attachment_url")
     private String attachmentUrl;
 
-    // @Column(value = "conversation_id")
-    // private Long conversationId;
-
     @CreatedDate
+    @Column(value = "created_at")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(value = "last_modified_at")
     private LocalDateTime lastModifiedAt;
+
+    @Transient
+    @Setter(value = AccessLevel.NONE)
+    private transient boolean isNew = true;
 
     public void setMessageId(Long messageId) {
         id.setMessageId(messageId);
@@ -59,5 +68,20 @@ public class Message implements Serializable {
 
     public Long getConversationId() {
         return id.getConversationId();
+    }
+
+    @Override
+    public boolean isNew() {
+        return this.isNew;
+    }
+
+    @PrePersist
+    @PostLoad
+    public void markNotNew() {
+        this.isNew = false;
+    }
+
+    public void markNew() {
+        this.isNew = true;
     }
 }
